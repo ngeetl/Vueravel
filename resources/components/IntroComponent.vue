b005163142e1e23bf9444e7c2a8d2f08c8a38414
yeni <ngeetl@naver.com>
2024-03-18 15:08:52 +0900

    intersection observer를 이용한 렌더링 이벤트 설정(fade-in)

--- resources/components/IntroComponent.vue
+++ resources/components/IntroComponent.vue
@@ -1,45 +1,62 @@
 <template>
-    <div class="intro h-lvh relative space px-40">
+    <div ref="observedElement" class="intro h-lvh relative space px-40">
         <div class="flex h-[90%] justify-center items-center container mx-auto">
-            <div class=" text-white absolute txt-shadow font-light">
+            <div ref="event1" class=" text-white absolute txt-shadow font-light">
                 <div class=" text-3xl">
-                Welcome! We're
+                  Welcome! We're
                 </div>
-                <div class=" text-8xl font-bold font-sans">SANDBURG</div>
+                <div  class=" text-8xl font-bold font-sans">SANDBURG</div>
                 <div class=" text-2xl mt-1">
                     국내 유일, 커머스와 핀테크를 잇는 데이터 전문기업 <br/>
                 </div>
             </div>
-            <div class="absolute">
+            <div ref="event2" class="absolute">
                 <dotlottie-player src="https://lottie.host/644d1237-0fb9-4f4b-881c-c6f491c18995/oedx7CtBW3.json" background="transparent" speed=".3" style="opacity: .2; filter: grayscale(100%); width: 550px; height: 550px;" loop autoplay></dotlottie-player>
             </div>
         </div>
-        <!-- 스크롤 버튼 -->
-        <!-- <div class="flex justify-center"> 
-
-            <button @click="scrollToBottom"><img src="../../public/icon-down.png" class="w-14 pulse over opacity-35" /></button>
-        </div> -->
-
-
     </div>
 </template>
 
 <script>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, onUnmounted } from 'vue';
 
 export default {
   name: 'IntroComponent',
   setup() {
-    // 스크롤 이벤트
-    const scrollToBottom = () => {
-      window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: "smooth",
-      });
-    };
+    // observer
+    const observedElement = ref(null);
+    const event1 = ref(null);
+    const event2 = ref(null);
 
-    return { scrollToBottom };
+    const enventHandler = () => {
+      const observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              event1.value.classList.add('fadeIn-up');
+              event2.value.classList.add('fadeIn-down');
+              observer.unobserve(entry.target); // 더 이상 관찰하지 않음
+            }
+          });
+        });
+  
+        if (observedElement.value) {
+          observer.observe(observedElement.value);
+        }
+    }
+
+    onMounted(() => {
+      enventHandler()
+    });
+
+    onUnmounted(() => {
+      if (observer) {
+        observer.disconnect();
+      }
+    });
+
+    return { observedElement, event1, event2 }
   },
+
 };
 </script>
 
@@ -50,30 +67,39 @@ export default {
     background: linear-gradient(180deg, rgba(16,0,41,1) 0%, rgba(23,50,122,1) 35%, rgb(88, 103, 189) 48%, rgb(20, 24, 100) 73%, rgb(5, 0, 18) 100%);
 }
  
-@keyframes pulse {
-  0% {
-    transform: scale(1); 
-  }
-  50% {
-    transform: scale(.8); 
-  }
-  100% {
-    transform: scale(1); 
-  }
-}
-
-.pulse {
-  animation: pulse 1.3s infinite; 
-}
-
-.over:hover {
-    animation: none;
-}
-
+/* text shadow */
 .txt-shadow {
     text-shadow: 2px 2px 16px rgba(23, 23, 23, 0.679);
 }
 
+/* event */
+.fadeIn-up {
+  animation: fadeIn-up 2.1s forwards; 
+}
+.fadeIn-down {
+  animation: fadeIn-down 2.1s forwards; 
+}
+
+@keyframes fadeIn-up {
+  from {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+@keyframes fadeIn-down {
+  from {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
 
 
 </style>
