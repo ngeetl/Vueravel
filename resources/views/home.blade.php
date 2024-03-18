5d821ec9bd2bb5ce65f845a556303c09a16aae6c
yeni <ngeetl@naver.com>
2024-03-18 18:29:38 +0900

    welcome.blade.php파일을 layout과 home으로 각각 나눔

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title')
+SANDBURG
+@endsection
+
+@section('content')
+@vite('resources/js/app.js') 
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,11 +1,11 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>SANDBURG</title>
+    <title>@yield('title', 'SANDBURG')</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
@@ -13,11 +13,12 @@
     <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=e60513e31042d98c0889764ff58ba286&libraries=services"></script>
 </head>
 <body>
-    <div class="bg-slate-50">  
-        <div id="app">
-            @vite('resources/js/app.js')
-        </div>
+<div class="bg-slate-50">  
+    <div id="app">
+    @yield('content')
     </div>
+</div>
+    
 <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
 </body>
-</html> -->
+</html>
